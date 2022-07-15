295208c3c61a0d6a9cf528b65169200487de6a0b
Paul <paul.iaszfalvi@gmail.com>
2022-07-15 18:26:06 -0400

    Updated contact page to hide on sent

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,12 @@
 import "./contact.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 const FORM_ENDPOINT = ""; // TODO - fill on the later step
 
 const Contact = () => {
   const form = useRef();
+  const [showForm, setShowForm] = useState(true);
 
   //const [submitted, setSubmitted] = useState(false);
   const handleSubmit = (e) => {
@@ -28,49 +29,59 @@ const Contact = () => {
       );
   };
 
+  const clicked = () => setShowForm(false);
+
   return (
     <div className="contactpage p-5 " id="contact">
-      <div className="formbox container">
-        <form
-          ref={form}
-          action={FORM_ENDPOINT}
-          onSubmit={handleSubmit}
-          method="POST"
-          target="_blank"
-        >
-          <div className="mb-3 pt-0">
-            <input
-              type="text"
-              placeholder="Your name"
-              name="name"
-              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-              required
-            />
-          </div>
-          <div className="mb-3 pt-0">
-            <input
-              type="email"
-              placeholder="E-mail"
-              name="email"
-              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-              required
-            />
-          </div>
-          <div className="mb-3 pt-0">
-            <textarea
-              placeholder="Your message"
-              name="message"
-              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-              required
-            />
-          </div>
-          <div className="mb-3 pt-0 text-center">
-            <button className="submit-button btn btn-primary" type="submit">
-              Send a message
-            </button>
-          </div>
-        </form>
-      </div>
+      {showForm ? (
+        <div className="formbox container">
+          <form
+            ref={form}
+            action={FORM_ENDPOINT}
+            onSubmit={handleSubmit}
+            method="POST"
+            target="_blank"
+          >
+            <div className="mb-3 pt-0">
+              <input
+                type="text"
+                placeholder="Your name"
+                name="name"
+                className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0">
+              <input
+                type="email"
+                placeholder="E-mail"
+                name="email"
+                className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0">
+              <textarea
+                placeholder="Your message"
+                name="message"
+                className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0 text-center">
+              <button
+                className="submit-button btn btn-primary"
+                type="submit"
+                onClick={clicked}
+              >
+                Send a message
+              </button>
+            </div>
+          </form>
+        </div>
+      ) : (
+        <div className="sentForm">Thank you, your message has been sent.</div>
+      )}
 
       <p className="portfolio-copyright text-white">
         Copyright © 2022 - Paul Iaszfalvi |{" "}
